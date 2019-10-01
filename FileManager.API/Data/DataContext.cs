44333ce9211ccfa98379b70ddf51c9ec23ef511c
Phil Mosely <moselyphil@gmail.com>
2019-10-01 16:09:02 -0400

    User In Process

--- FileManager.API/Data/DataContext.cs
+++ FileManager.API/Data/DataContext.cs
@@ -8,21 +8,25 @@ namespace datingapp.api.Data
         public DataContext(DbContextOptions<DataContext> options) : base (options){}
 
         public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<UserRole> UserRole { get; set; }
+
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
-            builder.Entity<Like>()
-                .HasKey(k => new {k.LikerId, k.LikeeId});
+            builder.Entity<UserRole>()
+                .HasKey(k => new {k.UserId, k.RoleId});
 
-            builder.Entity<Like>()
-                .HasOne(u => u.Likee)
-                .WithMany(u => u.Likers)
-                .HasForeignKey(u => u.LikeeId)
+            builder.Entity<UserRole>()
+                .HasOne(u => u.User)
+                .WithMany(u => u.Role)
+                .HasForeignKey(u => u.UserId)
                 .OnDelete(DeleteBehavior.Restrict);
             
-             builder.Entity<Like>()
-                .HasOne(u => u.Liker)
-                .WithMany(u => u.Likees)
-                .HasForeignKey(u => u.LikerId)
+             builder.Entity<UserRole>()
+                .HasOne(u => u.Role)
+                .WithMany(u => u.User)
+                .HasForeignKey(u => u.RoleId)
                 .OnDelete(DeleteBehavior.Restrict);
 
         }
---
+++ FileManager.API/Models/Role.cs
@@ -0,0 +1,8 @@
+namespace FileManager.API.Models
+{
+    public class Role
+    {
+        public int Id { get; set; }
+        public string RoleName { get; set; }
+    }
+}
